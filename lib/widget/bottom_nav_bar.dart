535b6bcf13caabe3dd8f114d41a56b1c8b5d2184
Nathan BAUD <yatherthan@gmail.com>
2025-11-17 08:13:04 +0100

    fusion style et routes

--- lib/widget/bottom_nav_bar.dart
+++ lib/widget/bottom_nav_bar.dart
@@ -9,7 +9,14 @@ class BottomNavBar extends StatelessWidget {
       left: 0,
       right: 0,
       bottom: 0,
-      child: Padding(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.green, // Fond vert
+          border: Border.all(
+            color: Colors.white,
+            width: 12, // Bordure blanche épaisse
+          ),
+        ),
         padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -18,22 +25,22 @@ class BottomNavBar extends StatelessWidget {
               onPressed: () {
                 Navigator.pushNamed(context, '/profile');
               },
-              icon: const Icon(Icons.person, color: Colors.white, size: 28),
+              icon: Icon(Icons.person, color: Colors.white, size: 28),
               tooltip: 'Profil',
             ),
             IconButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/home');
               },
-              icon: const Icon(Icons.home, color: Colors.white, size: 28),
+              icon: Icon(Icons.home, color: Colors.white, size: 28),
               tooltip: 'Accueil',
             ),
             IconButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/shop');
               },
-              icon: const Icon(Icons.store, color: Colors.white, size: 28),
-              tooltip: 'Boutique',
+              icon: Icon(Icons.store, color: Colors.white, size: 28),
+              tooltip: 'Magasin',
             ),
           ],
         ),
